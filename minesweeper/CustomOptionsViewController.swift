b7bc120ff25b966af743daad58eb005b11e050a6
Karl Matti <karlmatti@hotmail.com>
2020-04-30 12:39:41 +0300

    added settings ui, uisplitview, navigationview

---
+++ minesweeper/CustomOptionsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CustomOptionsViewController.swift
+//  minesweeper
+//
+//  Created by Karl Matti on 30.04.2020.
+//  Copyright © 2020 karlmatti. All rights reserved.
+//
+
+import UIKit
+
+class CustomOptionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func FieldSizeSlider(_ sender: UISlider) {
+       
+    }
+    @IBAction func BombsSlider(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ minesweeper/OptionsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OptionsViewController.swift
+//  minesweeper
+//
+//  Created by Karl Matti on 30.04.2020.
+//  Copyright © 2020 karlmatti. All rights reserved.
+//
+
+import UIKit
+
+class OptionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
